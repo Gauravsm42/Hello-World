c1cfb46a8c3dc0a431ea8d7193892589ed1254c9
Gaurav <gauravsm42@gmail.com>
2021-12-24 15:40:41 +0530

    Started learning about functions.

--- Functions.py
+++ Functions.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+phrase = "Draft code"  # Container
+print(phrase + " is\ncool")  # creates new line
+print(phrase.upper().isupper())
+print(phrase[0])  # indexing in most languages starts from 0
+print(phrase.index("D"))
+print(phrase.replace("Draft", "Zero"))
+print(phrase.replace("code", "elephant"))
+
+print(4 - 220 + 4.5)
+print((5 + 4) * 3)  # Python follows BODMAS
+print(5 + 4 * 3)
+print(10 % 3)
+my_num = 5
+print(my_num % 2)  # % (mod) divides the nos. and then prints the remainder
+print(str(my_num) + " is my favourite number")
+# str converts numbers into strings, you can't use numbers alongside strings without str
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hello-World
 This is a repository I am using for learning Python from scratch.  
 I had tried learning html and css 3 years ago. Got bored after couple months.  
-This time I want to atleast be able to complete one small project. Hopefully, putting this out there online will help me stay on track.  
+This time I want to at least be able to complete one small project. Hopefully, putting this out there online will help me stay on track.  
 ...  
 Although, I doubt anyone will see this.
